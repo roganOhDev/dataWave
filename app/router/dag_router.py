ab3da3a85366f71dae4e0a94601ca6f2651fabf2
roganOhDev <fdscbjdcnhd@gmail.com>
2021-12-28 01:34:50 +0900

    simplify dto location

--- app/router/dag_router.py
+++ app/router/dag_router.py
@@ -6,22 +6,22 @@ from starlette.responses import Response
 
 from app.common.database import db
 from app.domain.dag import dag_composite_service as composite_service
-from app.dto import dag_info_dto
+from app.dto.dag_info_dto import *
 
 router = APIRouter()
 
 
 @router.put("")
-def create(request: dag_info_dto.DagCreateDto, session: Session = Depends(db.session)):
+def create(request: DagCreateDto, session: Session = Depends(db.session)):
     return Response(content=composite_service.save(request, session), media_type="application/json")
 
 
-@router.put("/{uuid}", response_model=dag_info_dto.DagUpdateDto)
-def update(uuid: str, request: dag_info_dto.DagUpdateDto, session: Session = Depends(db.session)):
+@router.put("/{uuid}", response_model=DagUpdateDto)
+def update(uuid: str, request: DagUpdateDto, session: Session = Depends(db.session)):
     return Response(content=composite_service.update(uuid, request, session), media_type="application/json")
 
 
-@router.get("", response_model=dag_info_dto.DagInfoDto)
+@router.get("", response_model=DagInfoDto)
 def find(uuid: str, session: Session = Depends(db.session)):
     return composite_service.find(uuid, session)
 
