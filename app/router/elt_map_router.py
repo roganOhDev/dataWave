156dd0b3c1b7b09cc55438a7c1a598ab4b23ada5
roganOhDev <fdscbjdcnhd@gmail.com>
2022-01-01 00:31:21 +0900

    update/deactivate elt_map

--- app/router/elt_map_router.py
+++ app/router/elt_map_router.py
@@ -20,6 +20,16 @@ def create(request: EltMapSaveDto, session: Session = Depends(db.session)):
     composite_service.create(request, session)
 
 
+@router.put("/activate")
+def activate(uuid: str, session: Session = Depends(db.session)):
+    composite_service.activate(uuid, session)
+
+
+@router.put("/deactivate")
+def activate(uuid: str, session: Session = Depends(db.session)):
+    composite_service.deactivate(uuid, session)
+
+
 @router.put("/{uuid}")
 def update(uuid: str, request: EltMapSaveDto, session: Session = Depends(db.session)):
     composite_service.update(uuid, request, session)
@@ -28,13 +38,5 @@ def update(uuid: str, request: EltMapSaveDto, session: Session = Depends(db.sess
 @router.delete("")
 def delete(uuids: List[str] = Query(None), session: Session = Depends(db.session)):
     composite_service.delete(uuids, session)
-
-@router.put("/activate")
-def activate(uuid: str, request: EltMapSaveDto, session: Session = Depends(db.session)):
-    composite_service.activate(uuid, session)
-
-@router.put("/deactivate")
-def activate(uuid: str, request: EltMapSaveDto, session: Session = Depends(db.session)):
-    composite_service.deactivate(uuid, session)
 
 # TODO : active 할땐 dag 파일 만들고, deactive 할 땐 dag 파일 지우기 (파일 생성 코드 이후)
